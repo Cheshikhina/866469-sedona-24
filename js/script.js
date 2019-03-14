954e539124a23cb75373364184ea62e06ace594e
Daria <d.sumina@mail.ru>
2019-03-14 20:51:10 +0300

    внесла изменения
    переименовала файлы с min
    Б5 - Нужно удалить px у iframe - удалила
    Б5 - http://joxi.ru/12MjX4XIlk3x1A - Впринципе не ошибка, но по возможности поправь - исправила
    Б8 - http://joxi.ru/Grqg3K3c4G1Jy2 - убрала !important
    Б11 - http://joxi.ru/v29l9D9fZpEklm - Кажеться нужно добавить пару пикселей высоты - попыталась что-то сделать (но у меня при 100% норма ((( )
    Б11 - Нужен пробел - сделано
    Б12 - http://joxi.ru/82Q5yMyIw9Pqx2 - Исчезли звезды и обрезаеться цыфры IE11 - обрезку цифр убрала, звезды грузятся, но долго
    Б12 - http://joxi.ru/Q2K7O4OiLvzGD2 - IE11 - сделано
    Б15 - http://joxi.ru/8AnVZWZuzyBgBA - Нужно провести автоформатирование (alt+shift+F) - сделано
    
    Д15 - http://joxi.ru/krDDd4duK4168r - Подключена не минимизированая версия - подключила минимизированную
    Д20 - http://joxi.ru/BA0vgOguMp0z4m - Когда отключен javaScript форма должна быть открыта - сделано
    Д25 - http://joxi.ru/gmvgkOkcq1pzE2 - Это глобальные стили. Нужно добавить им класс. - исправила
    Д26 - На айфрейм с картой нужно поставить tabindex="-1" чтобі фокус туда не заходил. - сделано
    Д26 - http://joxi.ru/E2pglLlc7GdMEm - Нужно удалить href чтобы туда фокус не попадал - сделано
    
    Удалить коментированій код - http://joxi.ru/12MjX4XIlk3O1A - сделано
    Автоформатирование JS - сделано
    http://joxi.ru/Dr8Eb1bfoMVWWm - Форма не отправляеться, http://joxi.ru/LmGDZ4Zuwly982 - это ломает отправку - исправила

--- js/script.js
+++ js/script.js
@@ -1,28 +1,29 @@
+var closeningButton = document.querySelector(".user");
 var openingButton = document.querySelector(".hotel-search");
 var popup = document.querySelector(".user");
 var arrivalDate = popup.querySelector("[name=arrival-date]");
 var dateOfDeparture = popup.querySelector("[name=date-of-departure]");
 var adults = popup.querySelector("[name=adults]");
 
+closeningButton.classList.add("user-close");
+
 openingButton.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.toggle("user-open");
-    });
+});
 
 popup.addEventListener("submit", function (evt) {
-    evt.preventDefault();
     if (!arrivalDate.value || !dateOfDeparture.value || !adults.value) {
-    evt.preventDefault();
-    console.log("Нужно заполнить поле");
+        evt.preventDefault();
+        console.log("Нужно заполнить поле");
     }
-    });
+});
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-    if (popup.classList.contains("user-open")) {
-    evt.preventDefault();
-    popup.classList.remove("user-open");
-    }}
-    });
-
-    
+        if (popup.classList.contains("user-open")) {
+            evt.preventDefault();
+            popup.classList.remove("user-open");
+        }
+    }
+});
